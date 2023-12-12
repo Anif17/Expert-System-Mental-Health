a43823cdc0bc0bea91c1e1ae0eb8fcce331661d9
MUHAMMAD ANIF BIN ZULKIFLI <muhammadanif17@gmail.com>
2023-12-12 14:33:58 +0800

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/header/Header";
-import Home from "./pages/1.home/Home";
-import Service from "./pages/2.services/Service";
-import Portfolio from "./pages/3.portfolio/Portfolio";
-import Result from "./pages/4. result/Result";
+import Home from "./pages/home/Home";
+import Service from "./pages/services/Service";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Result from "./pages/result/Result";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -13,14 +13,12 @@ function App() {
       <div>
         <Router>
           <Navbar />
-
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/services" element={<Service />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/result" element={<Result />} />
+            <Route path="/Services" element={<Service />} />
+            <Route path="/Portfolio" element={<Portfolio />} />
+            <Route path="/Result" element={<Result />} />
           </Routes>
-
           <Footer />
         </Router>
       </div>
